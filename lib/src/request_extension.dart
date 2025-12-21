419637ed317c79e34e757e903b6222a8686237a0
Arindam Karmakar <connect@karindam.in>
2025-12-21 09:08:38 +0530

    Added essential extension methods to Request class

---
+++ lib/src/request_extension.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:shelf/shelf.dart';
+import 'package:shelf_essentials/src/form_data.dart';
+import 'package:shelf_essentials/src/http_method.dart';
+
+extension RequestExtension on Request {
+  /// Returns a [Stream] representing the body.
+  Future<String> body() async => await readAsString();
+
+  /// Returns a [Future] containing the form data as a [Map].
+  Future<FormData> formData() {
+    return parseFormData(headers: headers, body: body, bytes: read);
+  }
+
+  /// Returns a [Future] containing the body text parsed as a json object.
+  /// This object could be anything that can be represented by json
+  /// e.g. a map, a list, a string, a number, a bool...
+  Future<dynamic> json() async => jsonDecode(await body());
+
+  /// The [HttpMethod] associated with the request.
+  HttpMethod get httpMethod {
+    return HttpMethod.values.firstWhere(
+      (m) => m.value == method.toUpperCase(),
+      orElse: () => throw UnsupportedHttpMethodException(method),
+    );
+  }
+
+  /// Connection information for the associated HTTP request.
+  HttpConnectionInfo get connectionInfo {
+    return context['shelf.io.connection_info']! as HttpConnectionInfo;
+  }
+}
+
+/// {@template unsupported_http_method_exception}
+/// Exception thrown when an unsupported HTTP method is used.
+/// {@endtemplate}
+class UnsupportedHttpMethodException implements Exception {
+  /// {@macro unsupported_http_method_exception}
+  const UnsupportedHttpMethodException(this.method);
+
+  /// The unsupported http method.
+  final String method;
+
+  @override
+  String toString() =>
+      '''
+Unsupported HTTP method: $method. 
+The following methods are supported:
+${HttpMethod.values.map((m) => m.value.toUpperCase()).join(', ')}.''';
+}
